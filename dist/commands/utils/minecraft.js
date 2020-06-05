df6ff4fc931882d9244f537534a1d44e334e637b
Florian ERNST <florianernst59@gmail.com>
2020-06-05 21:23:31 +0200

    Removed dist file & went on develop branch

--- dist/commands/utils/minecraft.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toMcFunctionName = void 0;
-function toMcFunctionName(functionName) {
-    const [namespace, ...folders] = functionName;
-    return `${namespace}:${folders.join('/')}`;
-}
-exports.toMcFunctionName = toMcFunctionName;
-//# sourceMappingURL=minecraft.js.map
