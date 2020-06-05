422de4a31df4abd5039b6ae7a75aea97f69234af
Florian ERNST <florianernst59@gmail.com>
2020-06-05 21:56:42 +0200

    Removed hand-built dist folder

--- dist/variables/Coordinates.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type Relative = string | '~ ~ ~';
-export declare type Local = string | '^ ^ ^';
-export declare type Absolute = string | '0 0 0';
-export declare type Coordinates = Relative & Local & Absolute;
