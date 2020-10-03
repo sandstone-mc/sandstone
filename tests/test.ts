27c7c3662bf5b878ca3529daffa687ab1ab3bef3
Florian ERNST <florianernst59@gmail.com>
2020-10-03 16:27:01 +0200

    Change pack.mcmeta description; Added keywords for NPM

--- tests/test.ts
+++ tests/test.ts
@@ -18,3 +18,14 @@ mcfunction('cc', () => {
 })
 
 saveDatapack('My datapack', { verbose: true, world: 'Crea1_15' })
+
+/*
+
+Added Recipe, raw command, and updated README
+
+@TheMrZZ TheMrZZ released this now
+
+Added Recipe, a raw command that can be used to create custom commands, and updated README.
+Github Releases will now automatically be published to NPM.
+
+*/
