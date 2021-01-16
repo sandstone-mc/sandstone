496d61491b02d2f4c855168b4c4a71a12d3c1bf7
Florian ERNST <florianernst59@gmail.com>
2021-01-16 15:06:31 +0100

    Added BasePathClass & BasePathOptions to exports

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export * from './_internals/arguments'
+export { BasePathClass, BasePathOptions } from './_internals/datapack/BasePath'
 export { MCFunctionInstance } from './_internals/datapack/Datapack'
 export { LiteralUnion } from './_internals/generalTypes'
 export {
