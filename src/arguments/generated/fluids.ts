d785d885fc50f1e684b25adf361fa32a4540955e
nphhpn <nphhpnphnnhp@gmail.com>
2021-06-17 13:18:35 +0700

    Update auto-generated resources to 1.17, using mcdata repository

--- src/arguments/generated/fluids.ts
+++
@@ -1,1 +0,0 @@
-export type FLUIDS = 'minecraft:empty' | 'minecraft:flowing_water' | 'minecraft:water' | 'minecraft:flowing_lava' | 'minecraft:lava'
---
+++ src/arguments/generated/structure.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+/* Auto-generated */
+export type STRUCTURES = (
+  'pillager_outpost' |
+  'mineshaft' |
+  'mansion' |
+  'jungle_pyramid' |
+  'desert_pyramid' |
+  'igloo' |
+  'ruined_portal' |
+  'shipwreck' |
+  'swamp_hut' |
+  'stronghold' |
+  'monument' |
+  'ocean_ruin' |
+  'fortress' |
+  'endcity' |
+  'buried_treasure' |
+  'village' |
+  'nether_fossil' |
+  'bastion_remnant'
+)
