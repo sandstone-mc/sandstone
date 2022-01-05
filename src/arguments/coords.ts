65f8708d3cc2c1dfa41fd10ac88f76becd0003fa
Florian ERNST <florianernst59@gmail.com>
2022-01-05 23:24:40 +0100

    Added '~1 ^1 6' as a valid way to create coordinates. Fixes #134.

--- src/arguments/coords.ts
+++ src/arguments/coords.ts
@@ -1,5 +1,7 @@
 import type { VectorClass } from '@variables'
 
-export type Coordinates = [x: string, y: string, z: string] | VectorClass<[string, string, string]>
+type Coordinate = number | `${'' | '~' | '^'}${number}`;
+
+export type Coordinates = [x: string, y: string, z: string] | VectorClass<[string, string, string]> | `${Coordinate} ${Coordinate} ${Coordinate}`
 export type Rotation = [horizontal: string | VectorClass<[string]>, vertical: string | VectorClass<[string]>] | VectorClass<[string, string]>
-export type ColumnCoordinates = [x: string, z: string] | VectorClass<[string, string]>
+export type ColumnCoordinates = [x: string, z: string] | VectorClass<[string, string]> | `${Coordinate} ${Coordinate}`
