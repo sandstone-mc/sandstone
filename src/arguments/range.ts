d9ddfa9638b186684ef244bfbf134ef0b2a39201
Florian ERNST <florianernst59@gmail.com>
2022-01-09 15:00:28 +0100

    Fix #123 - Range now allow for Minecraft's string ranges

--- src/arguments/range.ts
+++ src/arguments/range.ts
@@ -1,1 +1,11 @@
-export type Range = number | [min: number, max: number] | [min: number, max: null | undefined] | [min: null | undefined, max: number] | [min: number]
+export type Range = (
+  | number
+  | [min: number, max: number]
+  | [min: number, max: null | undefined]
+  | [min: null | undefined, max: number]
+  | [min: number]
+  | `${number}`
+  | `${number}..`
+  | `..${number}`
+  | `${number}..${number}`
+)
