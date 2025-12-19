90f8c0e62d273ac1dc7c6c76dd180e6b0add942f
MulverineX <mulverin3@gmail.com>
2025-12-19 11:33:18 -0700

    💥 Remove old generated types

--- src/arguments/generated/dimension.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable */
-/* Auto-generated */
-export type DIMENSIONS = 'overworld' | 'the_end' | 'the_nether'
--- src/arguments/generated/fluid.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable */
-/* Auto-generated */
-export type FLUIDS = 'empty' | 'flowing_lava' | 'flowing_water' | 'lava' | 'water'
