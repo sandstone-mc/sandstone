8538a7ac7209d488fc6aaaa66aa04c83f2835b12
MulverineX <mulverin3@gmail.com>
2026-07-28 12:53:46 -0600

    ✨️ Fully implement `Flow#throw`, resolves #276

--- src/pack/pack.ts
+++ src/pack/pack.ts
@@ -171,6 +171,8 @@ import {
   SimplifyExecuteFunctionVisitor,
   SimplifyReturnRunFunctionVisitor,
   SwitchTransformationVisitor,
+  ThrowInlineVisitor,
+  ThrowPropagationVisitor,
   UnifyChainedExecutesVisitor,
 } from './visitors'
 import type { SymbolResource } from 'sandstone/arguments/generated/dispatcher'
@@ -1543,10 +1545,12 @@ export class SandstonePack {
         new SwitchTransformationVisitor(this),
         new OrTransformationVisitor(this),
         new IfElseTransformationVisitor(this),
+        new ThrowInlineVisitor(this),
         new ContainerCommandsToMCFunctionVisitor(this),
 
         // Special visitors
         new AwaitBodyVisitor(this),
+        new ThrowPropagationVisitor(this),
 
         // Optimization
         new OptimizeMacroTemporariesVisitor(this),
